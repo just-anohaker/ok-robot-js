55043de6b58a284bff0959003737e204b0893320
anohaker <anohaker@Mojave.lan>
2019-05-31 17:17:58 +0800

    添加事件通知交互机制

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ const { isElectronPlatform } = require("./lib/platform");
 module.exports = {
     isElectronPlatform,
     config: require("./lib/config"),
+    eventbus: require("./lib/platform/event-bus"),
     user: require("./lib/user.js")
 };
