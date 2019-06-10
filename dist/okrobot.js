3070a6d030b1ccc08515b37e2fcc1d4db9dc2f25
anohaker <anohaker@Mojave.lan>
2019-06-10 16:38:54 +0800

    添加接口

--- dist/okrobot.js
+++ dist/okrobot.js
@@ -277,6 +277,7 @@ const config = require("./config");
  * @param {number} options.incr -  价格增量百分比
  * @param {number} options.size - 挂单数量
  * @param {number} options.sizeIncr - 数量递增百分比
+ * @param {string} options.instrument_id - 交易对
  * @param {object} account 
  * @param {string} account.name - username
  * @param {string} account.httpKey - httpKey
@@ -328,6 +329,7 @@ async function generate(options, account) {
  * @param {number} options.type - 1: 买入; 2: 卖出
  * @param {number} options.topPrice - 交易最高价
  * @param {number} options.startPrice - 交易最低价
+ * @param {string} options.instrument_id - 交易对
  * @param {object} account 
  * @param {string} account.name - username
  * @param {string} account.httpKey - httpKey
@@ -364,6 +366,7 @@ async function cancel(options, account) {
  * @param {number} options.type - 1: 买入;  2: 卖出
  * @param {number} options.price - 价格
  * @param {number} options.size - 数量
+ * @param {string} options.instrument_id - 交易对
  * @param {object} account 
  * @param {string} account.name - username
  * @param {string} account.httpKey - httpKey
@@ -400,6 +403,7 @@ async function limitOrder(options, account) {
  * @param {number} options.type - 1: 买入; 2: 卖出
  * @param {number} options.notional - 买入时的金额
  * @param {number} options.size - 卖出时的数量
+ * @param {string} options.instrument_id - 交易对
  * @param {object} account 
  * @param {string} account.name - username
  * @param {string} account.httpKey - httpKey
@@ -432,7 +436,14 @@ async function marketOrder(options, account) {
 }
 /**
  * 
- * @param {*} account 
+ * @param {object} options 
+ * @param {number} options.type - 1: 买入; 2: 卖出
+ * @param {number} options.depth - 深度
+ * @param {number} options.size - 数量
+ * @param {number} options.perSize - 单笔数量
+ * @param {number} options.priceLimit - 价格限制
+ * @param {string} options.instrument_id - 交易对
+ * @param {object} account 
  * @param {string} account.name - username
  * @param {string} account.httpKey - httpKey
  * @param {string} account.httpSecret - httpSecret
@@ -440,12 +451,45 @@ async function marketOrder(options, account) {
  */
 
 
-async function startDepInfo(account) {
+async function icebergOrder(options, account) {
   let result;
 
   try {
     const data = {
+      options,
       account
+    };
+
+    if (platform.isElectronPlatform()) {
+      result = await platform.calllocal("batchorder.icebergOrder", data);
+    } else {
+      result = await platform.postremote(`${config.hostname}/api/batch_order/icebergOrder`, data);
+    }
+  } catch (error) {
+    console.log("[batchorder.icebergOrder] exception:", error);
+    throw error;
+  }
+
+  console.log("[batchorder.icebergOrder] response:", JSON.stringify(result));
+  return result;
+}
+/**
+ * 
+ * @param {*} options 
+ * @param {string} options.instrument_id - 交易对
+ * @param {string} options.name - username
+ * @param {string} options.httpKey - httpKey
+ * @param {string} options.httpSecret - httpSecret
+ * @param {string} options.passphrase -  passphrase
+ */
+
+
+async function startDepInfo(options) {
+  let result;
+
+  try {
+    const data = {
+      options
     };
 
     if (platform.isElectronPlatform()) {
@@ -461,15 +505,29 @@ async function startDepInfo(account) {
   console.log("[batchorder.startDepInfo] response:", JSON.stringify(result));
   return result;
 }
+/**
+ * 
+ * @param {*} options 
+ * @param {string} options.instrument_id - 交易对
+ * @param {string} options.name - username
+ * @param {string} options.httpKey - httpKey
+ * @param {string} options.httpSecret - httpSecret
+ * @param {string} options.passphrase -  passphrase
+ */
 
-async function stopDepInfo() {
+
+async function stopDepInfo(options) {
   let result;
 
   try {
+    const data = {
+      options
+    };
+
     if (platform.isElectronPlatform()) {
-      result = await platform.calllocal("batchorder.stopDepthInfo", undefined);
+      result = await platform.calllocal("batchorder.stopDepthInfo", data);
     } else {
-      result = await platform.postremote(`${config.hostname}/api/batch_order/stopDepInfo`, undefined);
+      result = await platform.postremote(`${config.hostname}/api/batch_order/stopDepInfo`, data);
     }
   } catch (error) {
     console.log("[batchorder.stopDepInfo] exception:", error);
@@ -479,6 +537,32 @@ async function stopDepInfo() {
   console.log("[batchorder.stopDepInfo] response:", JSON.stringify(result));
   return result;
 }
+/**
+ * 
+ * @param {object} options 
+ * @param {string} options.instrument_id - 交易对
+ * @param {number} options.from - 开始
+ * @param {number} options.to - 结束
+ * @param {number} options.limit - 限制
+ * @param {number} options.state - 订单状态
+ * <note>
+ * "-2":失败,
+ * "-1":撤单成功,
+ * "0":等待成交 ,
+ * "1":部分成交, 
+ * "2":完全成交,
+ * "3":下单中,
+ * "4":撤单中,
+ * "6": 未完成（等待成交+部分成交）,
+ * "7":已完成（撤单成功+完全成交）
+ * </note>
+ * @param {object} account 
+ * @param {string} options.name - username
+ * @param {string} options.httpKey - httpKey
+ * @param {string} options.httpSecret - httpSecret
+ * @param {string} options.passphrase -  passphrase
+ */
+
 
 async function getOrderData(options, account) {
   let result;
@@ -509,6 +593,7 @@ module.exports = {
   cancel,
   limitOrder,
   marketOrder,
+  icebergOrder,
   startDepInfo,
   stopDepInfo,
   getOrderData
