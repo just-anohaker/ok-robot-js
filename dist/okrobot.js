c1bcd5900278333a66e74ba3e429d5d74cf8b32c
anohaker <anohaker@Mojave.lan>
2019-06-13 18:44:18 +0800

    增加接口

--- dist/okrobot.js
+++ dist/okrobot.js
@@ -679,7 +679,7 @@ async function getSpotTrade(options) {
     };
 
     if (platform.isElectronPlatform()) {
-      result = await platform.calllocal("", data);
+      result = await platform.calllocal("okex_utils.getSpotTrade", data);
     } else {
       result = await platform.postremote(`${config.hostname}/api/batch_order/getTradeData`, data);
     }
@@ -701,7 +701,7 @@ async function getSpotCandles(options) {
     };
 
     if (platform.isElectronPlatform()) {
-      result = await platform.calllocal("", data);
+      result = await platform.calllocal("okex_utils.getSpotCandles", data);
     } else {
       result = await platform.postremote(`${config.hostname}/api/batch_order/getCandlesData`, data);
     }
@@ -716,54 +716,7 @@ async function getSpotCandles(options) {
 
 module.exports = {
   getSpotTrade,
-  getSpotCandles // /**
-  //  * 
-  //  * @param {object} options 
-  //  * @param {number} options.type - 1:买入; 2:卖出
-  //  * @param {number} options.toPrice - 目标价格
-  //  * @param {object} account 
-  //  * @param {string} account.name - username
-  //  * @param {string} account.httpKey - httpKey
-  //  * @param {string} account.httpSecret - httpSecret
-  //  * @param {string} account.passphrase -  passphrase
-  //  */
-  // async function generate(options, account) {
-  //     let result;
-  //     try {
-  //         const data = { options, account };
-  //         if (platform.isElectronPlatform()) {
-  //             result = await platform.calllocal("takeorder.generate", data);
-  //         } else {
-  //             result = await platform.postremote(`${config.hostname}/api/take_order/gen`, data);
-  //         }
-  //     } catch (error) {
-  //         console.log("[takeorder.generate] exception:", error);
-  //         throw error;
-  //     }
-  //     console.log("[takeorder.generate] response:", JSON.stringify(result));
-  //     return result;
-  // }
-  // async function start(oids) {
-  //     let result;
-  //     try {
-  //         const data = { client_oids: oids };
-  //         if (platform.isElectronPlatform()) {
-  //             result = await platform.calllocal("takeorder.start", data);
-  //         } else {
-  //             result = await platform.postremote(`${config.hostname}/api/take_order`, data);
-  //         }
-  //     } catch (error) {
-  //         console.log("[takeorder.start] exception:", error);
-  //         throw error;
-  //     }
-  //     console.log("[takeorder.start] response:", JSON.stringify(result));
-  //     return result;
-  // }
-  // module.exports = {
-  //     generate,
-  //     start
-  // };
-
+  getSpotCandles
 };
 
 },{"./config":6,"./platform":9}],8:[function(require,module,exports){
